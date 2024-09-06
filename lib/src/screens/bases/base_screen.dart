206b5140904798c35af0f34e3dbff8d1c5e5db9a
Rhogger <rhoggerrv@gmail.com>
2024-09-06 15:29:43 -0300

    :sparkles: feat: Implementado a tela base

---
+++ lib/src/screens/bases/base_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:kitanda/src/screens/home/home_tab.dart';
+import 'package:kitanda/src/screens/irrigation/irrigation_tab.dart';
+import 'package:kitanda/src/screens/plants/plants_tab.dart';
+
+class BaseScreen extends StatefulWidget {
+  const BaseScreen({super.key});
+
+  @override
+  State<BaseScreen> createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  int currentIndex = 0;
+  final pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        children: const [
+          HomeTab(),
+          IrrigationTab(),
+          PlantsTab(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+            pageController.jumpToPage(index);
+          });
+        },
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withOpacity(.5),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.sunny), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.water_drop), label: 'Regas'),
+          BottomNavigationBarItem(icon: Icon(Icons.spa), label: 'Plantas'),
+        ],
+      ),
+    );
+  }
+}
