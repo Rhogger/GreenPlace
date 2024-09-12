773d0120ed8ff89c2984d434053fc9c5e0ea568d
Rhogger <rhoggerrv@gmail.com>
2024-09-12 18:03:23 -0300

    :truck: chore: Movido de lugar

--- lib/src/components/tiles/plant_tile.dart
+++ lib/src/components/tiles/plant_tile.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:kitanda/src/config/theme.dart';
 
 class PlantTile extends StatelessWidget {
   final String imageUrl;
+  final Color tileBackground;
 
-  const PlantTile({super.key, required this.imageUrl});
+  const PlantTile(
+      {super.key, required this.imageUrl, required this.tileBackground});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 120,
+      padding: const EdgeInsets.all(8),
       decoration: BoxDecoration(
-        color: CustomColors.terciary.withOpacity(0.3),
+        color: tileBackground,
         borderRadius: BorderRadius.circular(20),
       ),
       child: Container(
